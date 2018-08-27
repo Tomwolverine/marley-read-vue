f04a26aca3e3a40575fdb39449153550b894f6b6
tom lee <tdroid99@gmail.com>
2018-08-27 13:13:58 -0600

    working header and main

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <img src="../assets/logo-marley-reads.jpeg" alt="logo">
+        <h1>Marley Reads</h1>
+    </header>    
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+header {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-content: space-between;
+    
+}
+
+header img {
+    grid-row: 1/3;
+    grid-column: 1/2;
+    height: 100px;
+    width: auto;
+}
+
+header h1 {
+    grid-row: 1/2;
+    grid-column: 2/3;
+    margin: 0;
+    color: chocolate;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <div class="home">
+        <Header />
+        <img class="book" src="../assets/logo.png" alt="">
+        <h2>Authors</h2>
+        <img class="person" src="../assets/logo.png" alt="">
+        <h2>Books</h2>
+    </div>
+</template>
+
+<script>
+import Header from './Header'
+export default {
+    name: 'Home',
+    components: {
+        Header
+    }
+    
+}
+</script>
+
+<style>
+.home {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+</style>
